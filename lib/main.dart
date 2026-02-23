68e2cf66a3d8e6955939ce24f3bfdac05cb92506
bizhab <smpaisyiyah01@gmail.com>
2026-02-24 01:53:13 +0800

    penambahan project url supabse beserta anon key pada file main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:persentasi_karya/admin/bottom_navigation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 // import 'package:persentasi_karya/admin/home_crud/home_admin.dart';
   
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://qwpubtwhjogiwelonorv.supabase.co', // URL berdasarkan referensi kunci Anda
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3cHVidHdoam9naXdlbG9ub3J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NTkyNTIsImV4cCI6MjA4NzQzNTI1Mn0.QVBvrxVV8jrBbZyklSQwjj1XqjmYOBcLT4szT1Ir5ms', // Paste Anon Key yang baru saja Anda kirim
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
