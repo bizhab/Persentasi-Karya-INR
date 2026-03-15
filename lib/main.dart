14dccfd3cf731de86bd79d5d755b8f439daaffdc
bizhab <smpaisyiyah01@gmail.com>
2026-03-15 21:46:04 +0800

    Revert "perubahan"
    This reverts commit 7421ed1295ff1b2f59d4a0be84c4e2ced462706a.

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: ".env");
-
   await Supabase.initialize(
   url: dotenv.env['SUPABASE_URL']!,
   anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
